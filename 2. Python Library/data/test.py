8778fa1617588b1e1e80aed8b2f7c7954b4addb5
yun <yunznag00@gmail.com>
2023-12-07 03:29:47 +0900

    파이썬 공부 내용

---
+++ 2. Python Library/data/test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(*args):
+    re = sum(args)
+    return re
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
